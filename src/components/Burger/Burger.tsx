a82a8873c7706724269b865ccd3455b4e70398f6
Zheng Hanming <hanming2033@gmail.com>
2018-04-21 11:56:49 +0800

    changed to recusive call istead of loop to add ingredients

---
+++ src/components/Burger/Burger.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import styled from '../../my-styled-components'
+import BurgerIngredient, { ingredientType } from './BurgerIngredient/BurgerIngredient'
+
+interface IburgerProps {
+  ingredients: {
+    salad?: number
+    bacon?: number
+    cheese?: number
+    meat?: number
+  }
+}
+
+const Burger = styled.div`
+  width: 100%;
+  margin: auto;
+  height: calc(100vw * 0.9);
+  /* overflow: scroll; */
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+
+  @media (min-width: 500px) and (min-height: 400px) {
+    width: 350px;
+    height: 300px;
+  }
+
+  @media (min-width: 500px) and (min-height: 401px) {
+    width: 450px;
+    height: 400px;
+  }
+
+  @media (min-width: 1000px) and (min-height: 700px) {
+    width: 700px;
+    height: 600px;
+  }
+`
+// recursive function to add ingredient types into an array
+const addToArray = (igType: ingredientType, num: number, arr: any[]) => {
+  arr.push(<BurgerIngredient key={igType + num} type={igType} />)
+  num--
+  if (num > 1) {
+    addToArray(igType, num, arr)
+  }
+}
+
+const burger: React.SFC<IburgerProps> = props => {
+  // takes the ingredients object from props and split it into arrays of ingredient
+  // Object.keys(props.ingredients): take the keys of an object
+  const burgerIngredients = Object.keys(props.ingredients).reduce((arr: any[], igType: ingredientType) => {
+    addToArray(igType, props.ingredients[igType], arr)
+    return arr
+  }, [])
+
+  return (
+    <Burger>
+      <BurgerIngredient type="bread-top" />
+      {burgerIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </Burger>
+  )
+}
+
+export default burger
--- src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -1,18 +1,33 @@
 import * as React from 'react'
+import Burger from '../../components/Burger/Burger'
 
 export interface IBurgerBuilderProps {
   a?: string
 }
 
 export interface IBurgerBuilderState {
-  a?: string
+  ingredients: {
+    salad?: number
+    bacon?: number
+    cheese?: number
+    meat?: number
+  }
 }
 
 export default class BurgerBuilder extends React.Component<IBurgerBuilderProps, IBurgerBuilderState> {
+  public state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 3,
+      meat: 1
+    }
+  }
+
   public render() {
     return (
       <>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Control</div>
       </>
     )
